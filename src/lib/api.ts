0b16d17f1e7a278f02deffb1853439a3a54971dd
Said mia <saidmia.dev@gmail.com>
2024-08-30 20:51:26 +0100

    add protection to roue message

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import type { AxiosInstance } from 'axios';
 export default function api() {
   console.log(process.env.API_BASE_URL, ' ikhan');
   const apiClient: AxiosInstance = axios.create({
-    baseURL: `${process.env.API_BASE_URL}`,
+    baseURL: process.env.API_BASE_URL ?`${process.env.API_BASE_URL}` : '',
     timeout: 60000,
     headers: {
       Accept: 'application/json',
