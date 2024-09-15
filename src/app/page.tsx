5bbc5a7cefeb54d965cea88dcc052b04d0b8ba3f
Said mia <saidmia.dev@gmail.com>
2024-09-15 14:03:26 +0100

    add  padding top to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       <section
         ref={homePageRef}
-        className='flex section flex-col h-full w-full items-center pt-6 sm:pt-20 px-4 sm:px-[7%]'
+        className='flex section flex-col h-full w-full items-center pt-4 sm:pt-20 px-4 sm:px-[7%]'
       >
         <HomePage
           scrollToContactMeCallBack={scrollToPage.bind(null, contactRef)}
